b6d37d5cba5d8321ec13d0a0e7c2224dcfdc8bbd
LuisUlloaC <luis.ulloa75360@gmail.com>
2024-05-20 20:10:35 -0400

    Fix: Fixes on Rental Plan model,  features and serializer

--- rental/rentalPlan/api.py
+++ rental/rentalPlan/api.py
@@ -2,29 +2,42 @@ from rest_framework import status
 from rest_framework.response import Response
 from settings.utils.api import APIViewWithPagination
 from rest_framework.permissions import IsAuthenticated
-from rental.rentalPlan.features import get_rental_plans, create_rental_plan, get_rental_plan, delete_rental_plan, update_rental_plan
-from rental.rentalPlan.serializer import CreateRentalPlanSerializer, RentalPlanSerializer, UpdateRentalPlanSerializer
+from rental.rentalPlan.features import (
+    get_rental_plans,
+    create_rental_plan,
+    get_rental_plan,
+    delete_rental_plan,
+    update_rental_plan,
+)
+from rental.rentalPlan.serializer import (
+    CreateRentalPlanSerializer,
+    RentalPlanSerializer,
+    UpdateRentalPlanSerializer,
+)
 from settings.utils.exceptions import BadRequest400APIException
 from rental.tenantUser.permissions import IsAdminTenantUser, IsAdminOrStaffTenantUser
 from rental.rentalPlan.exceptions import validate_plan_and_handle_errors
+
 
 class ListAndCreateRentalPlansView(APIViewWithPagination):
     permission_classes = [IsAuthenticated, IsAdminTenantUser]
 
     def get(self, request):
-        search_text = request.query_params.get('searchText', None)
-        order_by = request.query_params.get('orderBy', 'name')
-        asc = request.query_params.get('asc', None)
+        search_text = request.query_params.get("searchText", None)
+        order_by = request.query_params.get("orderBy", "name")
+        asc = request.query_params.get("asc", None)
 
         try:
             rental_plans_list = get_rental_plans(
                 search_text=search_text,
                 order_by=order_by,
-                asc=False if asc == 'False' else True
+                asc=False if asc == "False" else True,
             )
 
             paginator = self.pagination_class()
-            paginated_rental_plans = paginator.paginate_queryset(rental_plans_list, request)
+            paginated_rental_plans = paginator.paginate_queryset(
+                rental_plans_list, request
+            )
             serialized_list = RentalPlanSerializer(paginated_rental_plans, many=True)
             return paginator.get_paginated_response(serialized_list.data)
         except Exception as e:
@@ -35,21 +48,22 @@ class ListAndCreateRentalPlansView(APIViewWithPagination):
         validate_plan_and_handle_errors(serializer)
 
         created_rental_plan = create_rental_plan(
-            name=serializer.validated_data['name'],
-            amount=serializer.validated_data['amount'],
-            periodicity=serializer.validated_data['periodicity'],
+            name=serializer.validated_data["name"],
+            amount=serializer.validated_data["amount"],
+            periodicity=serializer.validated_data["periodicity"],
+            tenant=request.user.defaultTenantUser().tenant,
         )
 
         serialized_rental_plan = RentalPlanSerializer(created_rental_plan)
 
         return Response(serialized_rental_plan.data, status=status.HTTP_201_CREATED)
-    
+
 
 class GetUpdateAndDeleteARentalPlanView(APIViewWithPagination):
     permission_classes = [IsAuthenticated, IsAdminOrStaffTenantUser]
 
     def get_permissions(self):
-        if self.request.method in ['PUT', 'DELETE']:
+        if self.request.method in ["PUT", "DELETE"]:
             return [IsAuthenticated(), IsAdminTenantUser()]
         return super().get_permissions()
 
@@ -61,19 +75,14 @@ class GetUpdateAndDeleteARentalPlanView(APIViewWithPagination):
         return Response(serialized_rental_plan.data, status=status.HTTP_200_OK)
 
     def put(self, request, rental_plan_id):
-        serializer = UpdateRentalPlanSerializer(data={
-            'id': rental_plan_id,
-            'name': request.data.get('name'),
-            'amount': request.data.get('amount'),
-            'periodicity': request.data.get('periodicity'),
-        })
+        serializer = UpdateRentalPlanSerializer(data=request.data)
         validate_plan_and_handle_errors(serializer)
 
         updated_rental_plan = update_rental_plan(
             rental_plan_id=rental_plan_id,
-            name=serializer.validated_data.get('name'),
-            amount=serializer.validated_data.get('amount'),
-            periodicity=serializer.validated_data.get('periodicity'),
+            name=serializer.validated_data.get("name"),
+            amount=serializer.validated_data.get("amount"),
+            periodicity=serializer.validated_data.get("periodicity"),
         )
         serialized_rental_plan = RentalPlanSerializer(updated_rental_plan)
 
--- rental/rentalPlan/features.py
+++ rental/rentalPlan/features.py
@@ -8,13 +8,14 @@ def get_rental_plans(search_text: str = None, order_by: str = None, asc: bool = 
 
     if search_text:
         rental_plans = rental_plans.filter(
-            Q(name__icontains=search_text) |
-            Q(periodicity__icontains=search_text) |
-            Q(amount__icontains=search_text))
+            Q(name__icontains=search_text)
+            | Q(periodicity__icontains=search_text)
+            | Q(amount__icontains=search_text)
+        )
 
     if order_by:
         if not asc:
-            order_by = '-' + order_by
+            order_by = "-" + order_by
         rental_plans = rental_plans.order_by(order_by)
 
     return rental_plans
@@ -24,40 +25,48 @@ def get_rental_plan(rental_plan_id: int):
     try:
         rental_plan = RentalPlan.objects.get(id=rental_plan_id)
     except RentalPlan.DoesNotExist:
-        raise NotFound404APIException(
-            f'Rental Plan with id {rental_plan_id} not found')
+        raise NotFound404APIException(f"Rental Plan with id {rental_plan_id} not found")
 
     return rental_plan
 
 
-def create_rental_plan(name: str, amount: int, periodicity: str):
+def create_rental_plan(
+    name: str, 
+    amount: int, 
+    periodicity: str, 
+    tenant
+):
     new_rental_plan = RentalPlan.objects.create(
-            name=name,
-            amount=amount,
-            periodicity=periodicity
-        )
+        name=name, 
+        amount=amount, 
+        periodicity=periodicity, 
+        tenant=tenant
+    )
     new_rental_plan.save()
     return new_rental_plan
 
 
-def update_rental_plan(rental_plan_id: int, name: str = None, amount: int = None, periodicity: str = None):
-    try:
-        rental_plan = RentalPlan.objects.get(id=rental_plan_id)
-    except RentalPlan.DoesNotExist:
-        raise NotFound404APIException(
-            f'Rental Plan with id {rental_plan_id} not found')
+def update_rental_plan(
+    rental_plan_id: int, 
+    name: str = None, 
+    amount: int = None, 
+    periodicity: str = None
+):
+    rental_plan = RentalPlan.objects.get(id=rental_plan_id)
+    if rental_plan:
+        if name:
+            rental_plan.name = name
 
-    if name:
-        rental_plan.name = name
+        if amount:
+            rental_plan.amount = amount
 
-    if amount is not None:
-        rental_plan.amount = amount
+        if periodicity:
+            rental_plan.periodicity = periodicity
 
-    if periodicity is not None:
-        rental_plan.periodicity = periodicity
-
-    rental_plan.full_clean()
-    rental_plan.save()
+        rental_plan.full_clean()
+        rental_plan.save()
+    else:
+        raise NotFound404APIException(f"Rental Plan with id {rental_plan_id} not found")
     return rental_plan
 
 
@@ -67,5 +76,4 @@ def delete_rental_plan(rental_plan_id: int):
         rental_plan.delete()
 
     except RentalPlan.DoesNotExist:
-        raise NotFound404APIException(
-            f'Rental Plan with id {rental_plan_id} not found')
+        raise NotFound404APIException(f"Rental Plan with id {rental_plan_id} not found")
