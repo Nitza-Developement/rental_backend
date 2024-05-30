1e00575d6b49627b17b46b81e1c5449bd777d199
Vladimir <vladimir.rdguez@gmail.com>
2024-05-30 07:11:27 -0400

    Fixing a breake line

--- rental/user/exceptions.py
+++ rental/user/exceptions.py
@@ -9,8 +9,8 @@ class ErrorUserWithEmailAlreadyExists(APIException):
 
     def __init__(self, email: str):
         self.status_code = status.HTTP_400_BAD_REQUEST
-        self.default_detail = f'User with email {email} already exists'
-        self.default_code = 'user-001'
+        self.default_detail = f"User with email {email} already exists"
+        self.default_code = "user-001"
 
 
 class ErrorUserInvalidEmail(APIException):
@@ -18,61 +18,60 @@ class ErrorUserInvalidEmail(APIException):
     def __init__(self, email: str):
         self.status_code = status.HTTP_400_BAD_REQUEST
         self.default_detail = f'Invalid email "{email}"'
-        self.default_code = 'user-002'
+        self.default_code = "user-002"
 
 
 class ErrorUserInvalidName(APIException):
 
     def __init__(self, name_error_message: str):
         self.status_code = status.HTTP_400_BAD_REQUEST
-        self.default_detail = f'Error in name: {name_error_message}'
-        self.default_code = 'user-003'
+        self.default_detail = f"Error in name: {name_error_message}"
+        self.default_code = "user-003"
 
 
 class ErrorUserInvalidPassword(APIException):
 
     def __init__(self, password_error_message: str):
         self.status_code = status.HTTP_400_BAD_REQUEST
-        self.default_detail = f'Error in password: {password_error_message}'
-        self.default_code = 'user-005'
+        self.default_detail = f"Error in password: {password_error_message}"
+        self.default_code = "user-005"
 
 
 class ErrorUserInvalidProfilePicture(APIException):
 
     def __init__(self, image_error_message: str):
         self.status_code = status.HTTP_400_BAD_REQUEST
-        self.default_detail = f'Error in profile picture: {
-            image_error_message}'
-        self.default_code = 'user-006'
+        self.default_detail = f"Error in profile picture: {image_error_message}"
+        self.default_code = "user-006"
 
 
 def validate_user_and_handle_errors(serializer: Serializer):
 
     serializer.is_valid()
 
-    if 'email' in serializer.errors:
+    if "email" in serializer.errors:
 
-        error_detail: ErrorDetail = serializer.errors['email'][0]
+        error_detail: ErrorDetail = serializer.errors["email"][0]
 
-        if error_detail.code == 'unique':
-            raise ErrorUserWithEmailAlreadyExists(serializer.data.get('email'))
+        if error_detail.code == "unique":
+            raise ErrorUserWithEmailAlreadyExists(serializer.data.get("email"))
 
-        if error_detail.code in ['invalid', 'required', 'blank']:
-            raise ErrorUserInvalidEmail(serializer.data.get('email'))
+        if error_detail.code in ["invalid", "required", "blank"]:
+            raise ErrorUserInvalidEmail(serializer.data.get("email"))
 
-    if 'name' in serializer.errors:
+    if "name" in serializer.errors:
 
-        error_detail: ErrorDetail = serializer.errors['name'][0]
+        error_detail: ErrorDetail = serializer.errors["name"][0]
         raise ErrorUserInvalidName(str(error_detail))
 
-    if 'password' in serializer.errors:
+    if "password" in serializer.errors:
 
-        error_detail: ErrorDetail = serializer.errors['password'][0]
+        error_detail: ErrorDetail = serializer.errors["password"][0]
         raise ErrorUserInvalidPassword(str(error_detail))
 
-    if 'image' in serializer.errors:
+    if "image" in serializer.errors:
 
-        error_detail: ErrorDetail = serializer.errors['image'][0]
+        error_detail: ErrorDetail = serializer.errors["image"][0]
         raise ErrorUserInvalidProfilePicture(str(error_detail))
 
     if len(serializer.errors) > 0:
