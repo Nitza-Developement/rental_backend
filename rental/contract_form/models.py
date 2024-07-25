8db7875ad25b87ad3fd87f8bcea16d67f7771a6f
raulodev <raulodev@gmail.com>
2024-07-25 10:47:37 -0400

    create contract form template model

--- rental/contract_form/models.py
+++ rental/contract_form/models.py
@@ -1,1 +1,14 @@
+from django.db import models
+from rental.models import Tenant, TenantUser
 
+
+class ContractFormTemplate(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    user = models.ForeignKey(TenantUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    template = models.TextField()
+    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
