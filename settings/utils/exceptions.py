54d143ccd08599e2f540159175933fb124759670
Alberto <albertolicea00@icloud.com>
2024-07-27 16:05:38 -0400

    add exceptions response schemas

--- settings/utils/exceptions.py
+++ settings/utils/exceptions.py
@@ -1,6 +1,8 @@
+from rest_framework import serializers
+from drf_spectacular.utils import OpenApiResponse
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_401_UNAUTHORIZED
 from settings.utils.logging import log_error
 from traceback import format_tb
 
@@ -32,7 +34,16 @@ class BadRequest400APIException(APIException):
         self.status_code = HTTP_400_BAD_REQUEST
         self.default_detail = detail
         self.default_code = "bad_request"
+    class BadRequest400Schema(serializers.Serializer):
+        status_code = serializers.IntegerField()
+        default_detail = serializers.CharField(allow_null=True)
+        default_code = serializers.CharField(allow_null=True)
 
+    @staticmethod
+    def schema_response():
+        return OpenApiResponse(
+            response=BadRequest400APIException.BadRequest400Schema
+        )
 
 class NotFound404APIException(APIException):
 
@@ -41,3 +52,29 @@ class NotFound404APIException(APIException):
         self.status_code = HTTP_404_NOT_FOUND
         self.default_detail = detail
         self.default_code = "not_found"
+    
+    class NotFound404Schema(serializers.Serializer):
+        status_code = serializers.IntegerField()
+        default_detail = serializers.CharField(allow_null=True)
+        default_code = serializers.CharField(allow_null=True)
+
+    @staticmethod
+    def schema_response():        
+        return OpenApiResponse(response=NotFound404APIException.NotFound404Schema)
+
+class Unauthorized401APIException(APIException):
+
+    def __init__(self, detail: str = "Unauthorized access"):
+        super().__init__(detail)
+        self.status_code = HTTP_401_UNAUTHORIZED
+        self.default_detail = detail
+        self.default_code = "unauthorized"
+
+    class Unauthorized401Schema(serializers.Serializer):
+        status_code = serializers.IntegerField()
+        default_detail = serializers.CharField(allow_null=True)
+        default_code = serializers.CharField(allow_null=True)
+
+    @staticmethod
+    def schema_response():
+        return OpenApiResponse(response=Unauthorized401APIException.Unauthorized401Schema)
