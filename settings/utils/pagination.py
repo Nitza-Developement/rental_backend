acd41d87b3c965822b4ac28cd7ba9b299f61c053
Alberto <albertolicea00@icloud.com>
2024-07-27 16:05:14 -0400

    add paginated response schema

--- settings/utils/pagination.py
+++ settings/utils/pagination.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+from drf_spectacular.utils import inline_serializer, OpenApiResponse
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 
@@ -15,4 +17,23 @@ class DefaultPagination(PageNumberPagination):
                 "previous": self.get_previous_link(),
                 "results": data,
             }
+        )
+    
+    @staticmethod
+    def paginated_response_schema(serializer_class):
+        inner_serializer_class = (
+            serializer_class.child
+            if hasattr(serializer_class, "child")
+            else serializer_class
+        )
+        return OpenApiResponse(
+            response=inline_serializer(
+                name=f"Paginated{inner_serializer_class.__class__.__name__}",
+                fields={
+                    "count": serializers.IntegerField(),
+                    "next": serializers.IntegerField(allow_null=True),
+                    "previous": serializers.IntegerField(allow_null=True),
+                    "results": serializer_class,
+                },
+            )
         )
