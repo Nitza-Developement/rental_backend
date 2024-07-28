78999c46dde9167dff0f28e38f2221f0655eb96c
raulodev <raulodev@gmail.com>
2024-07-28 16:32:37 -0400

    update ContractFormTemplateAdmin model

--- rental/contract_form/models.py
+++ rental/contract_form/models.py
@@ -6,7 +6,7 @@ class ContractFormTemplate(models.Model):
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
     user = models.ForeignKey(TenantUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    template = models.TextField()
+    template = models.JSONField(null=True, blank=True, default=dict)
     is_active = models.BooleanField(default=True)
     name = models.CharField(max_length=255)
 
