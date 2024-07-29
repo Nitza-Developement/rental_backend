456f37476109f34a15045e6fd8dcfd096314e53e
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-07-28 20:19:56 -0400

    fix IsSelf permission in update profile
    This code allows to identify the user id in the post request and compare it with the one that is currently authenticated to confirm that they are the same

--- rental/user/permissions.py
+++ rental/user/permissions.py
@@ -3,4 +3,5 @@ from rest_framework import permissions
 
 class IsSelf(permissions.BasePermission):
     def has_permission(self, request, view):
-        return view.kwargs.get("user_id") == request.user.id
+        user_id=request.data.get("id")
+        return user_id and str(user_id) == str(request.user.id)
